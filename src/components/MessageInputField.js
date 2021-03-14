bc1511c79bc77f37917da0600a34a17795511255
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2021-03-15 05:29:12 +0900

    style: apply grid row to MessageList and MessageInputField components

--- src/components/MessageInputField.js
+++ src/components/MessageInputField.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles'
 
+const useStyles = makeStyles({
+  root: {
+    gridRow: 2,
+  }
+})
 const MessageInputField = (props) => {
+  const classes = useStyles();
   return (
-    <div>
+    <div className={classes.root}>
       <p>MessageInputField</p>
       <p>{props.name}</p>
     </div>
