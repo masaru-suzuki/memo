7b759032e94d684e2c31aa62e6a2faba1646dd51
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2021-03-14 15:15:18 +0900

    docs: add config.json for divide page (signIn, Main)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, {useState} from "react";
 import Main from "./Main";
 import SignIn from "./SignIn";
+import config from "../config.json"
+
 
 export default () => {
   const [name, setName] = useState('');
   // console.log({name});
-  if(name === '') {
+  if(config.signInEnabled && name === '') {//signInの状態も入れる
     return <SignIn setName={setName} />
   } else {
     return <Main name={name}/>
