188b579d4235325a78d640d9024189041e3531b5
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2021-03-14 14:59:01 +0900

    feat: divide 2component from App.js (Main Sign in)

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ import SignIn from "./SignIn";
 
 export default () => {
   const [name, setName] = useState('');
-  console.log({name});
-  name === '' ? <SignIn setName={setName}/> : <Main name={name}/>;
+  // console.log({name});
+  if(name === '') {
+    return <SignIn setName={setName} />
+  } else {
+    return <Main name={name}/>
+  }
 };
