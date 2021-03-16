0274222b23c0767c4e55d51540826c9d85a16f01
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2021-03-16 10:28:30 +0900

    remove hard cord(firebase config) and move .env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,26 @@
 import firebase from 'firebase'
+
+  //import .env fileここで環境変数を読み込む
+  const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PRIJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID,
+    REACT_APP_FIREBASE_MEASUREMENT_ID
+  } = process.env
+
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyCLe5N6y8eek1PNQDv5EUSZtd0OJlNyVMc",
-    authDomain: "message-exchange-app.firebaseapp.com",
-    projectId: "message-exchange-app",
-    storageBucket: "message-exchange-app.appspot.com",
-    messagingSenderId: "420096056913",
-    appId: "1:420096056913:web:8b4afb7d59f44908486270",
-    measurementId: "G-52XCHJ9SVV"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PRIJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID,
+    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
   };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
