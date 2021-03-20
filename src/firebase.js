f839c73c0fd0e595fe4dffbc8c9478ba883e24e1
masaru-suzuki <masaru.suzuki.yamagata@gmail.com>
2021-03-21 05:15:40 +0900

    feat: generate .env file autematically

--- src/firebase.js
+++ src/firebase.js
@@ -1,32 +1,34 @@
 import firebase from 'firebase'
 
-  //import .env fileここで環境変数を読み込む
-  const {
-    REACT_APP_FIREBASE_API_KEY,
-    REACT_APP_FIREBASE_AUTH_DOMAIN,
-    REACT_APP_FIREBASE_PRIJECT_ID,
-    REACT_APP_FIREBASE_STORAGE_BUCKET,
-    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    REACT_APP_FIREBASE_APP_ID,
-    REACT_APP_FIREBASE_MEASUREMENT_ID
-  } = process.env
+//import .env fileここで環境変数を読み込む
+const {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_DATABASE_URL,
+  REACT_APP_FIREBASE_PRIJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  REACT_APP_FIREBASE_APP_ID,
+  REACT_APP_FIREBASE_MEASUREMENT_ID,
+} = process.env
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: REACT_APP_FIREBASE_API_KEY,
-    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: REACT_APP_FIREBASE_PRIJECT_ID,
-    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: REACT_APP_FIREBASE_APP_ID,
-    measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID
-  };
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: REACT_APP_FIREBASE_PRIJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
+  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
+}
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig)
 
 //これでfirebaseのデータベースにアクセスできる
-const database = firebase.database();
+const database = firebase.database()
 
 //保存先の指定（保存名の指定）
 export const messagesRef = database.ref('messages')
@@ -34,6 +36,6 @@ export const messagesRef = database.ref('messages')
 export const pushMessagesToFirebase = (name, text) => {
   messagesRef.push({
     name: name,
-    message: text
+    message: text,
   })
 }
